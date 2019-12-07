6d465daae3bbc42b9b5a1fccee11323d4020039b
Nachiketa <nachiketa0333@gmail.com>
2019-12-07 18:05:15 -0500

    Initial check-in of module Final_Project

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    float maxSpawnRateInSecond = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ScheduleEnemySpawner();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnEnemy()
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        //GameObject anEnemy = (GameObject)Instantiate(EnemyGo);
+        //anEnemy.transform.position = new Vector2(Random.Range(min.x, max.x), max.y);
+
+        ScheduleNexrEnemySpawn();
+    }
+
+    void ScheduleNexrEnemySpawn()
+    {
+        float spawnInNSeconds;
+
+        if (maxSpawnRateInSecond > 1f)
+        {
+            spawnInNSeconds = Random.Range(1f, maxSpawnRateInSecond);
+        }
+        else
+        {
+            spawnInNSeconds = 1f;
+        }
+
+        InvokeSpawnEnemy(spawnInNSeconds);
+    }
+
+    void InvokeSpawnEnemy(float inSeconds)
+    {
+        Invoke("SpawnEnemy", inSeconds);
+    }
+
+    void IncreaseSpawnRate()
+    {
+        if (maxSpawnRateInSecond > 1f)
+        {
+            maxSpawnRateInSecond--;
+        }
+        else
+        {
+            CancelInvoke("IncreaseSpawnRate");
+        }
+    }
+
+    public void ScheduleEnemySpawner()
+    {
+        InvokeSpawnEnemy(maxSpawnRateInSecond);
+        InvokeRepeating("IncreaseSpawnRate", 0f, 30f);
+    }
+
+    public void UnscheduleEnemySpawner()
+    {
+        CancelInvoke("SpawnEnemy");
+        CancelInvoke("IncreaseSpawnRate");
+    }
+}
